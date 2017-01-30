b0237fd0a614aa07bf3bcfc41fc57e396c45dc32
Header-Develop <kritawitbunket@gmail.com>
2017-01-30 20:43:34 +0700

    This's Example Project

---
+++ ProgressBarFun/ProgressBarView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressBarView.swift
+//  ProgressBarFun
+//
+//  Created by Header-Develop on 1/29/2560 BE.
+//  Copyright © 2560 Header-Development. All rights reserved.
+//
+
+import UIKit
+
+class ProgressBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
